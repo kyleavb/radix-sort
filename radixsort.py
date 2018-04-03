51a3add00c674347c1eff6483ef62f64b62598ce
Kyle Van Bergen <kyleavb@gmail.com>
2018-04-02 21:37:23 -0700

    initial setup

---
+++ radixsort.py
@@ -0,0 +1,9 @@
+# 1) Do following for each digit i where i varies from least significant digit to the most significant digit.
+# ………….a) Sort input array using counting sort (or any stable sort) according to the i’th digit.
+
+def radix(arr):
+    print(arr)
+
+sort_list = [170, 45, 75, 90, 802, 24, 2, 66]
+print(radix(sort_list))
+print('Yo')
